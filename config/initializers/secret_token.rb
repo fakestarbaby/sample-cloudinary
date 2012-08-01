cdb4f818f5019b40e3e799c0c709738c5e9dcaa8
fakestarbaby <fakestarbaby@gmail.com>
2012-08-01 23:51:16 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleCloudinary::Application.config.secret_token = '279352544b3c8a77dc984c9348a36d3ec3172a754f956dfad5414d749567b5c1ba8001ec9ba91777850acbdad602f2a04216631f6adae1186f7dcc312d478917'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleCloudinary::Application.config.session_store :cookie_store, key: '_sample-cloudinary_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleCloudinary::Application.config.session_store :active_record_store
